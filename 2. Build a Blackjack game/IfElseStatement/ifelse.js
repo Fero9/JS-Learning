86df3cab72d2730844a7399820e7af254fdb9d1a
Fero9 <felixmulti00@gmail.com>
2023-06-12 14:44:40 +0200

    Added some more if statements to the blackjack files as well as updated the gode

---
+++ 2. Build a Blackjack game/IfElseStatement/ifelse.js
@@ -0,0 +1,23 @@
+let age = 22
+
+if(age < 21){
+    console.log("Sorry mate, you're not old enough to enter!")
+}
+else{
+    console.log("Welcome to the club, G!\n")
+}
+
+let newAge = 100
+
+if(newAge < 100)
+{
+    console.log("Sorry, you're not a 100 years old yet.")
+}
+else if(newAge === 100)
+{
+    console.log("Congrats! You're a 100 years old, here's your birthday card!")
+}
+else
+{
+    console.log("Hot diggity damn, you are more than a 100 years old?? Great job!")
+}

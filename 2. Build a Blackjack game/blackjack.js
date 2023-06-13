20659dba3cf106e2c893b05d588f8a43669ce49d
Fero9 <felixmulti00@gmail.com>
2023-06-13 08:01:56 +0200

    Polished the Blackjack game

--- 2. Build a Blackjack game/blackjack.js
+++ 2. Build a Blackjack game/blackjack.js
@@ -1,5 +1,6 @@
 let firstCard = 10
 let secondCard = 9
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 
 let hasBlackjack = false
@@ -7,19 +8,38 @@ let isAlive = true
 
 let message = ""
 
-if(sum <= 20)
-{
-    message = "Do you want to draw a new card? 🙂"
-}
-else if(sum === 21)
-{
-    message = "Wohoo! You've got Blackjack! 🥳"
-    hasBlackjack = true
-}
-else
-{
-    message = "You're out of the game! 😭"
-    isAlive = false
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardEl = document.getElementById("card-el")
+
+function startGame(){
+    playGame()
 }
 
-console.log(message)
+function playGame(){
+    sumEl.textContent = "Sum: " + sum
+    cardEl.textContent = "Cards: " + cards[0] + " and " + cards[1]
+    
+    if(sum <= 20)
+    {
+        message = "Do you want to draw a new card?"
+    }
+    else if(sum === 21)
+    {
+        message = "You've got Blackjack!"
+        hasBlackjack = true
+    }
+    else
+    {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+function newCard(){
+    console.log("Drawing a new card from the deck")
+    let newCard = 4
+    sum = sum + newCard
+    playGame()
+}

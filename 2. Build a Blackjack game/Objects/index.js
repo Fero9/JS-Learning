da84904ee02db47fd958af0f09d23718175da614
Fero9 <felixmulti00@gmail.com>
2023-06-14 08:39:07 +0200

    Finished the Blackjack game

---
+++ 2. Build a Blackjack game/Objects/index.js
@@ -0,0 +1,12 @@
+// Create an object that represents an airbnb castle listing.
+// It should contain at least one boolean, one string, one number, and one array
+// Log out at least two of the keys using the dot notation
+
+let castle = {
+    isRentable: true,
+    cName: "King's Castle",
+    age: 120,
+    rooms: ["Bedroom","Bathroom","Kitchen","Dining Room"]
+}
+
+console.log(castle.cName, castle.age)
